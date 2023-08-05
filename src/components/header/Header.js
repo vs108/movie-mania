cc8be2ca5bd699854568bf88dcc269aebd59c255
VIKAS SINGH <113005914+vs108@users.noreply.github.com>
2023-08-05 21:39:16 +0530

    Add files via upload

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://yt3.googleusercontent.com/ytc/AOPolaTl7DGRQKlYp6ZGzkc_zH4FxHG9BmcgTHV7Ocqe=s900-c-k-c0x00ffffff-no-rj"/></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
